894cf5384754688f3832824530c90e0a92c3a630
Sathish Thiyagarajan <sathish.thiyagarajan@earlham.ac.uk>
2022-06-16 19:02:33 +0100

    add dummy fa file and adjust coords in test

--- test_modBAM_tools.py
+++ test_modBAM_tools.py
@@ -19,24 +19,38 @@ class TestDetectToModBAMSuite(unittest.TestCase):
         cls.fakeDetect = ("# dummy data adapted from dnascent docs\n"
             "#Genome dummy.fasta\n"
             "#Index dummy.index\n"
-            ">5d10eb9a-aae1-4db8-8ec6-7ebb34d32575 chrXIII 72319 72327 fwd\n"
-            "72319\t0.017496\tTCGTTT\n"
-            "72322\t0.029483\tTTTCTG\n"
-            "72323\t0.039008\tTTCTGT\n"
-            "72324\t0.031474\tTCTGTG\n"
-            "72326\t0.026997\tTGTGAG\n"
-            ">a4f36092-b4d5-47a9-813e-c22c3b477a0c chrXVI 899273 899321 fwd\n"
-            "899276\t0.866907\tTCAAAT\n"
-            "899281\t0.947935\tTCCACA\n"
-            "899300\t0.014683\tTGGGAG\n"
-            "899312\t0.186812\tTAACGG\n"
-            "899320\t0.934850\tTTATTG\n"
-            ">c6785e1f-10d2-49cb-8ca3-e8d48979001b chrXIII 74003 81176 rev\n"
-            "74010\t0.012874\tTCTCTA\n"
-            "74011\t0.012428\tCTCTAA\n"
-            "74014\t0.016811\tTAACGA\n"
-            "74017\t0.013372\tCGACCA\n"
-            "74018\t0.013836\tGACCAA\n"
+            ">5d10eb9a-aae1-4db8-8ec6-7ebb34d32575 dummyI 9 17 fwd\n"
+            "9\t0.017496\tTCGTTT\n"
+            "12\t0.029483\tTTTCTG\n"
+            "13\t0.039008\tTTCTGT\n"
+            "14\t0.031474\tTCTGTG\n"
+            "16\t0.026997\tTGTGAG\n"
+            ">a4f36092-b4d5-47a9-813e-c22c3b477a0c dummyIII 273 321 fwd\n"
+            "276\t0.866907\tTCAAAT\n"
+            "281\t0.947935\tTCCACA\n"
+            "300\t0.014683\tTGGGAG\n"
+            "312\t0.186812\tTAACGG\n"
+            "320\t0.934850\tTTATTG\n"
+            ">c6785e1f-10d2-49cb-8ca3-e8d48979001b dummyII 3 36 rev\n"
+            "10\t0.012874\tTCTCTA\n"
+            "11\t0.012428\tCTCTAA\n"
+            "14\t0.016811\tTAACGA\n"
+            "17\t0.013372\tCGACCA\n"
+            "18\t0.013836\tGACCAA\n"
+            )
+
+        cls.fakeFaFile = (">dummyI\n"
+            "AGCTAGCTATCGTTTCTGTGAG\n"
+            ">dummyII\n"
+            "AGCTAGCTAGTCTCTAACGACCAA\n"
+            ">dummyIII\n"
+            "CCACACCACACCCACACACCCACACACCACACCACACACCACACCACACC\n"
+            "CCACACCACACCCACACACCCACACACCACACCACACACCACACCACACC\n"
+            "CCACACCACACCCACACACCCACACACCACACCACACACCACACCACACC\n"
+            "CCACACCACACCCACACACCCACACACCACACCACACACCACACCACACC\n"
+            "CCACACCACACCCACACACCCACACACCACACCACACACCACACCACACC\n"
+            "CCACACCACACCCACACACCCACACACTCAAATCCACACCACACCACACC\n"
+            "TGGGAGCCACCATAACGGCCTTATTG\n"
             )
 
     def test_get_gaps_in_base_pos(self):
@@ -61,11 +75,11 @@ class TestDetectToModBAMSuite(unittest.TestCase):
             },
             {
                 "readID": "5d10eb9a-aae1-4db8-8ec6-7ebb34d32575",
-                "refContig": "chrXIII",
-                "refStart": 72319,
-                "refEnd": 72327,
+                "refContig": "dummyI",
+                "refStart": 9,
+                "refEnd": 17,
                 "strand": "fwd",
-                "posOnRef": [72319, 72322, 72323, 72324, 72326],
+                "posOnRef": [9, 12, 13, 14, 16],
                 "probBrdU": [0.017496, 0.029483, 0.039008, 
                     0.031474, 0.026997],
                 "sixMerOnRef": ["TCGTTT", "TTTCTG", "TTCTGT", 
@@ -73,11 +87,11 @@ class TestDetectToModBAMSuite(unittest.TestCase):
             },
             {
                 "readID": "a4f36092-b4d5-47a9-813e-c22c3b477a0c",
-                "refContig": "chrXVI",
-                "refStart": 899273,
-                "refEnd": 899321,
+                "refContig": "dummyIII",
+                "refStart": 273,
+                "refEnd": 321,
                 "strand": "fwd",
-                "posOnRef": [899276, 899281, 899300, 899312, 899320],
+                "posOnRef": [276, 281, 300, 312, 320],
                 "probBrdU": [0.866907, 0.947935, 0.014683, 
                     0.186812, 0.934850],
                 "sixMerOnRef": ["TCAAAT", "TCCACA", "TGGGAG", 
@@ -85,11 +99,11 @@ class TestDetectToModBAMSuite(unittest.TestCase):
             },
             {
                 "readID": "c6785e1f-10d2-49cb-8ca3-e8d48979001b",
-                "refContig": "chrXIII",
-                "refStart": 74003,
-                "refEnd": 81176,
+                "refContig": "dummyII",
+                "refStart": 3,
+                "refEnd": 36,
                 "strand": "rev",
-                "posOnRef": [74010,74011,74014,74017,74018],
+                "posOnRef": [10, 11, 14, 17, 18],
                 "probBrdU": [0.012874, 0.012428, 0.016811, 
                     0.013372, 0.013836],
                 "sixMerOnRef": ["TCTCTA", "CTCTAA", "TAACGA", 
